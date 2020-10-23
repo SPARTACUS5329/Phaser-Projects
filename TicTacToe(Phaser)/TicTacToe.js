fd0bc48e09d94a8500525667a25ec964399a5570
SPARTACUS5329 <58896813+SPARTACUS5329@users.noreply.github.com>
2020-10-23 22:56:45 +0530

    Removed unnecessary code
    Just removed unnecessary code.

--- TicTacToe(Phaser)/TicTacToe.js
+++ TicTacToe(Phaser)/TicTacToe.js
@@ -114,9 +114,6 @@ class Progress extends Phaser.Scene {
         }
 
 
-
-
-
     }
 
     checkWin() {
@@ -542,12 +539,6 @@ const gameState = {
     relations: { 1: [0, 0], 2: [0, 1], 3: [0, 2], 4: [1, 0], 5: [1, 1], 6: [1, 2], 7: [2, 0], 8: [2, 1], 9: [2, 2] },
     cycle: 0
 };
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 var arr;
 var Xcoords;
